f8622b301f3f52b1e2a9e314184c3d6baae7e5d8
Ashika Gupta <ashikagupta1503@gmail.com>
2025-09-11 23:40:48 +0530

    Time: 5 ms (56.43%), Space: 45.3 MB (6.29%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int romanToInt(String s) {
+    
+        int n = s.length();
+        int total = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        for (int i = 0; i < n; i++) {
+            int value = map.get(s.charAt(i));
+
+            // If a smaller value is placed before a bigger value → subtract it (like IV = 4).
+            // Otherwise, add it (like VI = 6)
+            
+            if (i < n - 1 && value < map.get(s.charAt(i + 1))) {
+                total -= value;
+            } else {
+                total += value;
+            }
+        }
+        return total;
+    }
+}
