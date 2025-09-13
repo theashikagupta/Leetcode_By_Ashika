2b4a04e6c5ebbce4561aa0122d634d746c2e6a3b
Ashika Gupta <ashikagupta1503@gmail.com>
2025-09-13 21:58:46 +0530

    Time: 1 ms (100%), Space: 44.9 MB (16%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        // Reverse both lists
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        int carry = 0;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        // Traverse both lists
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+            carry = sum / 10;
+
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        // Handle leftover carry
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        // Reverse result to get forward order
+        return reverse(dummy.next);
+    }
+
+    // Helper function to reverse linked list
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
