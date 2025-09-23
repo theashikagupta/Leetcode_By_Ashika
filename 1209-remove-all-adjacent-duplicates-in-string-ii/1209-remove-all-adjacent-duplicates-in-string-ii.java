e3a5750620c5342b505a088600e419cdb276728a
Ashika Gupta <ashikagupta1503@gmail.com>
2025-09-23 17:20:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -9,25 +9,21 @@ class Solution {
     }
 
     public String removeDuplicates(String s, int k) {
-        Stack<Pair> st = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-            if (!st.isEmpty() && st.peek().ch == ch) {
+        Stack<Pair> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(!st.isEmpty() && st.peek().ch==ch){
                 st.peek().freq++;
-                if (st.peek().freq == k) {
-                    st.pop(); 
+                if(st.peek().freq==k){
+                    st.pop();
                 }
-            } else {
-                st.push(new Pair(ch, 1));
+            }else{
+                st.push(new Pair(ch,1));
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-        while (!st.isEmpty()) {
-            Pair p = st.pop();
-            for (int i = 0; i < p.freq; i++) {
-                sb.append(p.ch);
-            }
+        StringBuilder sb=new StringBuilder();
+        int size=st.size();
+        for(int i=0;i<size;i++ ){
+            sb.append(st.pop().ch);
         }
         return sb.reverse().toString();
     }
