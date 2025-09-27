237a63ced7c8019cd0342a71c9a344f04a09b825
Ashika Gupta <ashikagupta1503@gmail.com>
2025-09-27 16:59:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,14 @@
 # Write your MySQL query statement below
+    #INNER JOIN 
+-- SELECT today.id 
+-- FROM Weather as today
+-- INNER JOIN Weather as tomorrow
+-- ON DATE_SUB(today.recordDate,INTERVAL 1 DAY)=tomorrow.recordDate
+-- WHERE today.temperature>tomorrow.temperature;
+
+    #SELF JOIN
 SELECT today.id 
 FROM Weather as today
-INNER JOIN Weather as tomorrow
-ON DATE_SUB(today.recordDate,INTERVAL 1 DAY)=tomorrow.recordDate
+JOIN Weather as tomorrow
+ON DATEDIFF(today.recordDate,INTERVAL 1 DAY)=tomorrow.recordDate
 WHERE today.temperature>tomorrow.temperature;
