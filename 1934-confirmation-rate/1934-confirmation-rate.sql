da936175ab379dc7b6804d28c8bbc9e60ea30353
Ashika Gupta <ashikagupta1503@gmail.com>
2025-09-30 13:10:37 +0530

    Time: 710 ms (28.61%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,10 +1,12 @@
 SELECT 
     s.user_id,
-    ROUND(
-        IFNULL(SUM(c.action = 'confirmed') / COUNT(c.action), 0),
-        2
-    ) AS confirmation_rate
+    ROUND( IFNULL(SUM(c.action = 'confirmed') / COUNT(c.action), 0),2) AS confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c
     ON s.user_id = c.user_id
 GROUP BY s.user_id;
+
+
+
+
+
