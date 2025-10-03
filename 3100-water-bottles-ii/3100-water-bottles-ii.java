23869e7165c4a000c568424ff46ccf104ffbc926
Ashika Gupta <ashikagupta1503@gmail.com>
2025-10-03 23:40:31 +0530

    Time: 0 ms (100%), Space: 40.7 MB (84.46%) - LeetHub

--- 3100-water-bottles-ii/3100-water-bottles-ii.java
+++ 3100-water-bottles-ii/3100-water-bottles-ii.java
@@ -1,19 +1,29 @@
 class Solution {
     public int maxBottlesDrunk(int numBottles, int numExchange) {
-        int fullBottles=0;
-        int emptyBottles=numBottles;
-        int bottlesDrunk=numBottles;
-        while(emptyBottles>=numExchange){
-            while(emptyBottles>=numExchange){
-                emptyBottles=emptyBottles-numExchange*1;
-                fullBottles++;
-                numExchange++;
-            }
-             bottlesDrunk+=fullBottles;
-             emptyBottles+=fullBottles;
-             fullBottles=0;
-        }
-       
-     return bottlesDrunk;
+//   Method-1
+    // int bottlesDrunk = numBottles;
+    // int emptyBottles = numBottles;
+
+    // while (emptyBottles >= numExchange) {
+    //     int newFull = emptyBottles / numExchange; // bottles we get by exchange
+    //     bottlesDrunk += newFull;
+    //     emptyBottles = emptyBottles % numExchange + newFull; // remaining empties + new fulls drank
+    // }
+
+    // return bottlesDrunk;
+// }
+
+// Method-2
+    int ans=numBottles;
+    while(numBottles>=numExchange){
+        numBottles-=numExchange-1;
+        numExchange++;
+        ans++;
+    }
+    return ans;
+
+//Method-3
+    // if(numExchange == 1) return Integer.MAX_VALUE; // infinite loop scenario
+    // return numBottles + (numBottles - 1) / (numExchange - 1);
     }
 }
