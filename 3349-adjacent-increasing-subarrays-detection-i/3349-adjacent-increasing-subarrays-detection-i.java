d6dd5d1c903445d5bcda8b1d66584ba097fd25a6
Ashika Gupta <ashikagupta1503@gmail.com>
2025-10-15 00:44:11 +0530

    Time: 1 ms (100%), Space: 44.4 MB (80.88%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
@@ -1,16 +1,42 @@
 public class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
-        int n = nums.size();
-        int inc = 1, prevInc = 0, maxLen = 0;
-        for (int i = 1; i < n; i++) {
-            if (nums.get(i) > nums.get(i - 1)) inc++;
-            else {
-                prevInc = inc;
-                inc = 1;
-            }
-            maxLen = Math.max(maxLen, Math.max(inc >> 1, Math.min(prevInc, inc)));
-            if (maxLen >= k) return true;
+    //     int n = nums.size();
+    //     for(int start=0;start+2*k<=n;start++){
+    //         boolean first=isInc(nums,start,start+k);
+    //         boolean second=isInc(nums,start+k,start+2*k);
+    //         if(first && second){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+        
+    // }
+    // public boolean isInc(List<Integer> nums,int s,int e){
+    //     for(int i=s+1;i<e;i++){
+    //         if(nums.get(i)<=nums.get(i-1)){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+
+
+    int n=nums.size();
+    int currLen=1;
+    int prevLen=0;
+    for(int i=1;i<n;i++){
+        if(nums.get(i)>nums.get(i-1)){ //increasing order
+           currLen++;
+        }else{   //order break hogya
+            prevLen=currLen;
+            currLen=1;
         }
-        return false;
+        if(currLen>=2*k){
+            return true;
+        }
+        if(Math.min(currLen,prevLen)>=k){
+            return true;
+        }
+    }
+    return false;
     }
 }
