af3f346d2216a38e339dc9cf3dc261fdce8b0c67
Ashika Gupta <ashikagupta1503@gmail.com>
2025-10-15 08:32:36 +0530

    Time: 0 ms (100%), Space: 41.3 MB (32.07%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,20 +15,29 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        List<Integer> pPre=new ArrayList<>();
-        List<Integer> qPre=new ArrayList<>();
-        preorder(p,pPre);
-        preorder(q,qPre);
-        return pPre.equals(qPre);
-    }
+    //     List<Integer> pPre=new ArrayList<>();
+    //     List<Integer> qPre=new ArrayList<>();
+    //     preorder(p,pPre);
+    //     preorder(q,qPre);
+    //     return pPre.equals(qPre);
+    // }
 
-    public void preorder(TreeNode root, List<Integer> list) {
-        if(root==null) {
-        list.add(null);
-        return ;}
+    // public void preorder(TreeNode root, List<Integer> list) {
+    //     if(root==null) {
+    //     list.add(null);
+    //     return ;}
 
-        list.add(root.val);
-        preorder(root.left,list);
-        preorder(root.right,list);
+    //     list.add(root.val);
+    //     preorder(root.left,list);
+    //     preorder(root.right,list);
+
+
+    //if both root are null ,they are same
+    if(p==null && q==null) return true;
+    if(p==null || q==null) return false;
+    
+    if(p.val!=q.val) return false;
+    return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    
     }
 }
