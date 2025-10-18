1801d63d8fdf95255e20c9c0fee54a33efd33be4
Ashika Gupta <ashikagupta1503@gmail.com>
2025-10-18 11:55:04 +0530

    Time: 10 ms (97.24%), Space: 58.1 MB (73.79%) - LeetHub

--- 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.java
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.java
@@ -1,21 +1,37 @@
 class Solution {
     public int maxDistinctElements(int[] nums, int k) {
         
-        Arrays.sort(nums);
-        int count=0; int preMax=Integer.MIN_VALUE;
-        for(int n:nums){
-            int lowerBound=n-k;
-            int upperBound=n+k;
-            if(preMax <lowerBound ){
-                preMax=lowerBound;
-                count++;
-            }else if(preMax < upperBound ){
-                preMax++;
-                count++;
-            }
+        // Arrays.sort(nums);
+        // int count=0; int preMax=Integer.MIN_VALUE;
+        // for(int n:nums){
+        //     int lowerBound=n-k;
+        //     int upperBound=n+k;
+        //     if(preMax <lowerBound ){
+        //         preMax=lowerBound;
+        //         count++;
+        //     }else if(preMax < upperBound ){
+        //         preMax++;
+        //         count++;
+        //     }
 
             
+        // }
+        // return count;
+
+
+       
+        if (nums.length <= (k << 1) + 1) return nums.length;
+        Arrays.sort(nums);
+        int distinct = 0;
+        int l = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int m = Math.max(l+1, nums[i]-k);
+            if (m <= nums[i]+k) {
+                distinct++;
+                l = m;
+            }
         }
-        return count;
+        return distinct;
     }
 }
+  
