2de7fda16aace9f1bc56508af700dad7e85f6a6a
Ashika Gupta <ashikagupta1503@gmail.com>
2025-10-27 15:35:33 +0530

    Time: 7 ms (99.83%), Space: 45.2 MB (59.97%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,19 +1,42 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int prev=0;
-        int res=0;
-        for(String s:bank){
-            int current=0;
-            for(char c:s.toCharArray()){
-                if(c=='1'){
-                    current+=1;
-                }
-            }
-            if(current>0){
-                res+=prev*current;
-                prev=current;
-            }
+        // int prev=0;
+        // int res=0;
+        // for(String s:bank){
+        //     int current=0;
+        //     for(char c:s.toCharArray()){
+        //         if(c=='1'){
+        //             current+=1;
+        //         }
+        //     }
+        //     if(current>0){
+        //         res+=prev*current;
+        //         prev=current;
+        //     }
+        // }
+        // return res;
+
+        
+        int prevRowCount = 0;
+        int total=0;
+
+        for(String row : bank) {
+            int curRowCount = calc(row);
+            if(curRowCount==0) 
+                continue;
+
+            total += curRowCount * prevRowCount;
+            prevRowCount = curRowCount;
         }
-        return res;
+        return total;
     }
+
+    private int calc(String s) {
+        int count = 0;
+        for(char c : s.toCharArray()) 
+            count += c - '0';
+
+        return count;
+    }    
 }
+  
