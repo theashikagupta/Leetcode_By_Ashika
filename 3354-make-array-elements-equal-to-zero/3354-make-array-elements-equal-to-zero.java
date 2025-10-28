f38a4e36a4c6d777740ed5b0b25717ecc5c4a323
Ashika Gupta <ashikagupta1503@gmail.com>
2025-10-28 15:25:37 +0530

    Time: 129 ms (10.24%), Space: 43 MB (22.89%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
@@ -1,14 +1,35 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-        int len=nums.length, count=0, left=0;
-        int right=Arrays.stream(nums).sum();
+        int len=nums.length, res=0;
         for (int i=0; i<len; i++) {
-            left+=nums[i];
-            right-=nums[i];
-            if (nums[i]!=0) continue;
-            if (left==right) count+=2;
-            if (Math.abs(left-right) == 1) count++;
+            if(nums[i]==0){
+                if(allZeroes(nums,1,i)){
+                    res+=1;
+                }
+                if(allZeroes(nums,-1,i)){
+                    res+=1;
+                }
+            }
         }
-        return count;
+        return res;
+    }
+    private boolean allZeroes(int[] nums, int dir, int start){
+        int[] arr=nums.clone();
+        int direction=dir;
+        int curr=start;
+
+        while(curr>=0 && curr<arr.length){
+            if(arr[curr]==0){
+                curr+=direction;
+            }else{
+                arr[curr]-=1;
+                direction=-direction;
+                curr+=direction;
+            }
+        }
+        for(int x:arr){
+            if(x!=0) return false;
+        }
+        return true;
     }
 }
