8585a6ee031fdd76eabdfb47197450541a61cb5b
Ashika Gupta <ashikagupta1503@gmail.com>
2025-10-29 20:01:19 +0530

    Time: 0 ms (100%), Space: 40.9 MB (46.73%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
@@ -1,17 +1,22 @@
 class Solution {
    
     public int smallestNumber(int n) {
-        //O(n)
-        int x=n;
-        while((x & (x + 1))!=0){
-            x++;
-        }
-        return x;
+        // //O(n)
+        // int x=n;
+        // while((x & (x + 1))!=0){
+        //     x++;
+        // }
+        // return x;
+
         //O(logn)
         // int res=1;
         // while(res<n){
         //     res=2*res+1;
         // }
         // return res;
+
+       
+        int bits = (int)(Math.log(n) / Math.log(2)) + 1;  // number of bits needed
+        return (1 << bits) - 1;  // bitmask with all bits = 1
     }
 }
