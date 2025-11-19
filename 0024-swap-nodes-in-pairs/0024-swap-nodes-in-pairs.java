3503d9a90bc327d6b042d871073b72c7f5faeca0
Ashika Gupta <ashikagupta1503@gmail.com>
2025-11-19 10:01:14 +0530

    Time: 0 ms (100%), Space: 43 MB (15.07%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -13,29 +13,42 @@ class Solution {
         if(head==null || head.next==null){
             return head;
         }
-        ListNode dummy=new ListNode(-1);
-        dummy.next=head;
-        ListNode prev=dummy;
-        ListNode first=head;
-        ListNode sec=first.next;
-        ListNode third;
-        while(first!=null && sec!=null){
-            third=sec.next;
-            sec.next=first;
-            first.next=third;
-            if(prev!=null){
-                prev.next=sec;
-            }else{
-                head=sec;
-            }
-            prev=first;
-            first=third;
-            if(third!=null){
-                sec=third.next;
-            }else{
-                sec=null;
-            }
-        }
-        return dummy.next;
+
+        ListNode firstNode=head;
+        ListNode secondNode =head.next;
+        ListNode newHead=swapPairs(secondNode.next);
+        secondNode.next=firstNode;
+        firstNode.next=newHead;
+        
+        return secondNode;
+
+
+
+//         ListNode dummy=new ListNode(-1);
+//         dummy.next=head;
+//         ListNode prev=dummy;
+//         ListNode first=head;
+//         ListNode sec=first.next;
+//         ListNode third;
+//         while(first!=null && sec!=null){
+//             third=sec.next;
+//             sec.next=first;
+//             first.next=third;
+//             if(prev!=null){
+//                 prev.next=sec;
+//             }else{
+//                 head=sec;
+//             }
+//             prev=first;
+//             first=third;
+//             if(third!=null){
+//                 sec=third.next;
+//             }else{
+//                 sec=null;
+//             }
+//         }
+//         return dummy.next;
     }
 }
+
+
