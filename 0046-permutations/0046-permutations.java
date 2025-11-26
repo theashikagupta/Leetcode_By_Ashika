3bc1193f67adf780c13850f9ca0297792607a166
Ashika Gupta <ashikagupta1503@gmail.com>
2025-11-26 20:29:54 +0530

    Time: 0 ms (100%), Space: 45.2 MB (36.82%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,31 +1,107 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        boolean[] visited=new boolean[nums.length];
-        backtrack(nums,new ArrayList<>(),ans,visited);
-        return ans;
+        List<List<Integer>> res=new ArrayList<>();
+        solve(nums,res,0);
+        return res;
     }
-    private void backtrack(int[] nums,List<Integer> temp,List<List<Integer>> ans,boolean[] visited){
+    public void solve(int[] nums,List<List<Integer>> res,int idx ){
         int n=nums.length;
-        //base case
-        if(temp.size()==n){
-            ans.add(new ArrayList<>(temp));
+        if(idx==n){
+            List<Integer> temp = new ArrayList<>();
+            for (int x : nums) temp.add(x);
+            res.add(temp);
             return;
         }
-        for(int i=0;i<n;i++){
-        
-            if(!visited[i]){
-            //do
-              visited[i]=true;
-              temp.add(nums[i]);
-              //explore
-            backtrack(nums,temp,ans,visited);
-            //undo
-            visited[i]=false;
-            temp.remove(temp.size()-1);
-            }
-        
+        for(int i=idx;i<n;i++){
+            swap(nums,i,idx);
+            solve(nums,res,idx+1);
+           swap(nums,i,idx);
         }
-        
+
     }
+    public void swap(int[] nums,int i,int idx){
+         int temp=nums[i];
+         nums[i]=nums[idx];
+         nums[idx]=temp;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //O(n!)
+    //     List<List<Integer>> ans=new ArrayList<>();
+    //     boolean[] visited=new boolean[nums.length];
+    //     backtrack(nums,new ArrayList<>(),ans,visited);
+    //     return ans;
+    // }
+    // private void backtrack(int[] nums,List<Integer> temp,List<List<Integer>> ans,boolean[] visited){
+    //     int n=nums.length;
+    //     //base case
+    //     if(temp.size()==n){
+    //         ans.add(new ArrayList<>(temp));
+    //         return;
+    //     }
+    //     for(int i=0;i<n;i++){
+        
+    //         if(!visited[i]){
+    //         //do
+    //           visited[i]=true;
+    //           temp.add(nums[i]);
+    //           //explore
+    //            backtrack(nums,temp,ans,visited);
+    //           //undo
+    //            visited[i]=false;
+    //            temp.remove(temp.size()-1);
+    //         }
+        
+    //     }
+        
+    // }
 }
