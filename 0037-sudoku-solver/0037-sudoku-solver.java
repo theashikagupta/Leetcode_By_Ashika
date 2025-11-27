bef224712997c0a5f9d740472f94b9d5986f35ac
Ashika Gupta <ashikagupta1503@gmail.com>
2025-11-27 12:17:20 +0530

    Time: 331 ms (41.75%), Space: 42.8 MB (17.04%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,48 +1,37 @@
 class Solution {
-
-    private boolean solved = false;
-
     public void solveSudoku(char[][] board) {
         solve(board, 0, 0);
     }
 
-    private void solve(char[][] board, int row, int col) {
-        if (solved) return;  // stop all recursion
+    private boolean solve(char[][] board, int row, int col) {
+        int n = board.length;
 
-        if (row == 9) {      
-            solved = true;   // Sudoku completed
-            return;
-        }
+        if (row == n) return true;
 
-        int nextRow = (col == 8) ? row + 1 : row;
-        int nextCol = (col == 8) ? 0 : col + 1;
+        int nextRow = (col == n - 1) ? row + 1 : row;
+        int nextCol = (col == n - 1) ? 0 : col + 1;
 
-        if (board[row][col] != '.') {
-            solve(board, nextRow, nextCol);
-            return;
-        }
+        if (board[row][col] != '.') 
+            return solve(board, nextRow, nextCol);
 
         for (char num = '1'; num <= '9'; num++) {
             if (isSafe(board, row, col, num)) {
                 board[row][col] = num;
-
-                solve(board, nextRow, nextCol);
-
-                if (solved) return;  // do NOT undo if solved
-
-                board[row][col] = '.';  // backtrack
+                if (solve(board, nextRow, nextCol)) 
+                    return true;
+                board[row][col] = '.';
             }
         }
+
+        return false;
     }
 
     private boolean isSafe(char[][] board, int r, int c, char num) {
         for (int i = 0; i < 9; i++) {
             if (board[r][i] == num) return false;
             if (board[i][c] == num) return false;
-
             int boxRow = 3 * (r / 3) + i / 3;
             int boxCol = 3 * (c / 3) + i % 3;
-
             if (board[boxRow][boxCol] == num) return false;
         }
         return true;
