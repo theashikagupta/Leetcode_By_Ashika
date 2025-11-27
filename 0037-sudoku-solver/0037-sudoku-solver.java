73b2c3fd951cd2f487a36d970724aee77da9b742
Ashika Gupta <ashikagupta1503@gmail.com>
2025-11-27 12:15:48 +0530

    Time: 332 ms (41.6%), Space: 42.7 MB (24.39%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,37 +1,48 @@
 class Solution {
+
+    private boolean solved = false;
+
     public void solveSudoku(char[][] board) {
         solve(board, 0, 0);
     }
 
-    private boolean solve(char[][] board, int row, int col) {
-        int n = board.length;
+    private void solve(char[][] board, int row, int col) {
+        if (solved) return;  // stop all recursion
 
-        if (row==n) return true;
+        if (row == 9) {      
+            solved = true;   // Sudoku completed
+            return;
+        }
 
-        int nextRow = (col==n-1) ? row+1:row;
-        int nextCol = (col==n-1) ? 0:col+1;
+        int nextRow = (col == 8) ? row + 1 : row;
+        int nextCol = (col == 8) ? 0 : col + 1;
 
-        if (board[row][col]!='.') 
-            return solve(board, nextRow, nextCol);
+        if (board[row][col] != '.') {
+            solve(board, nextRow, nextCol);
+            return;
+        }
 
         for (char num = '1'; num <= '9'; num++) {
             if (isSafe(board, row, col, num)) {
                 board[row][col] = num;
-                if (solve(board, nextRow, nextCol)) 
-                    return true;
-                board[row][col] = '.';
+
+                solve(board, nextRow, nextCol);
+
+                if (solved) return;  // do NOT undo if solved
+
+                board[row][col] = '.';  // backtrack
             }
         }
-
-        return false;
     }
 
     private boolean isSafe(char[][] board, int r, int c, char num) {
         for (int i = 0; i < 9; i++) {
             if (board[r][i] == num) return false;
             if (board[i][c] == num) return false;
-            int boxRow = 3 *(r/3) + i/3;
-            int boxCol = 3 *(c/3) + i%3;
+
+            int boxRow = 3 * (r / 3) + i / 3;
+            int boxCol = 3 * (c / 3) + i % 3;
+
             if (board[boxRow][boxCol] == num) return false;
         }
         return true;
