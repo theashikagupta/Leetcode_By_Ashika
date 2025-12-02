989a2bc8862d693ab3a587c7fe06417f61afcf6f
Ashika Gupta <ashikagupta1503@gmail.com>
2025-12-02 10:13:32 +0530

    Time: 0 ms (100%), Space: 41.8 MB (45.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,29 @@
 class Solution {
+    int[] dp;
     public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+        //Bottom-up
+    //     int dp[]=new int[n+1];
+    //     dp[0]=1;
+    //     dp[1]=1;
+    //     for(int i=2;i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
       
-     return dp[n]; 
+    //  return dp[n]; 
+
+    //Top-down
+        dp = new int[n+1];       // create dp once
+        Arrays.fill(dp, -1);
+        return solve(n);
+    }
+
+    private int solve(int n) {
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+
+        if (dp[n] != -1) return dp[n];
+
+        dp[n] = solve(n - 1) + solve(n - 2);
+        return dp[n];
     }
 }
