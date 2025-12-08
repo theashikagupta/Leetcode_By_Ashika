1cfe07e98f079f614b6cdda2749b0f790cfd1243
Ashika Gupta <ashikagupta1503@gmail.com>
2025-12-08 21:17:09 +0530

    Time: 1 ms (99.09%), Space: 42.7 MB (44.07%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,18 +1,33 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
         //Approach-1 (Sorting)
-        List<Integer> list=new ArrayList<>();
+        // List<Integer> list=new ArrayList<>();
+        // for(int stone:stones){
+        //     list.add(stone);
+        // }
+        // while(list.size()>1){
+        //     Collections.sort(list);
+        //     int y=list.get(list.size()-1);
+        //     list.remove(list.size()-1);
+        //     int x=list.get(list.size()-1);
+        //     list.remove(list.size()-1);
+        //     list.add(Math.abs(y-x));
+        // }
+        // return list.isEmpty()?0:list.get(0);
+
+        //Approach-2 (Max-Heap)
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
         for(int stone:stones){
-            list.add(stone);
+            pq.add(stone);
         }
-        while(list.size()>1){
-            Collections.sort(list);
-            int y=list.get(list.size()-1);
-            list.remove(list.size()-1);
-            int x=list.get(list.size()-1);
-            list.remove(list.size()-1);
-            list.add(Math.abs(y-x));
+
+        while(pq.size()>1){
+            int y=pq.poll();
+            int x=pq.poll();
+            if(y!=x){
+                pq.add(Math.abs(y-x));
+            }
         }
-        return list.isEmpty()?0:list.get(0);
+        return pq.isEmpty()?0:pq.peek();
     }
 }
