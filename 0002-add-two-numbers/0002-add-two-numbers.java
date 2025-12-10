f40ff1ac774713aba1279720ca6bc9281bbf1542
Ashika Gupta <ashikagupta1503@gmail.com>
2025-12-10 10:05:41 +0530

    Time: 1 ms (99.99%), Space: 46.7 MB (7.57%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -38,20 +38,5 @@ class Solution {
         return dummy.next;
     }
     
-    public ListNode reverse(ListNode head) {
     
-
-    ListNode curr = head;
-    ListNode prev = null;
-    ListNode next;
-
-    while (curr != null) {
-        next = curr.next;   
-        curr.next = prev;   
-        prev = curr;        
-        curr = next;       
-    }
-
-    return prev; 
-    }
 }
