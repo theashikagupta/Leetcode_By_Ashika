79eda5b4d9d1e8131e9f104465626ec16b42843c
Ashika Gupta <ashikagupta1503@gmail.com>
2025-12-19 15:50:53 +0530

    Time: 416 ms (58.52%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,16 +1,31 @@
+-- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- BEGIN
+-- DECLARE M INT;
+-- SET M=N-1;
+--   RETURN (
+      
+--           (
+--               SELECT DISTINCT salary
+--               FROM Employee
+--               ORDER BY salary DESC
+--               LIMIT 1 OFFSET M
+--           )
+--   );
+-- END
+
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-DECLARE M INT;
-SET M=N-1;
+  DECLARE M INT;
+  SET M=N-1;
   RETURN (
-      SELECT IFNULL(
-          (
-              SELECT DISTINCT salary
-              FROM Employee
-              ORDER BY salary DESC
-              LIMIT 1 OFFSET M
-          ),
-          NULL
-      )
+    WITH RankedSalaries AS (
+      SELECT DISTINCT salary
+      FROM Employee
+      ORDER BY salary DESC
+    )
+    SELECT salary
+    FROM RankedSalaries
+    LIMIT 1 OFFSET M
   );
-END
+END;
+
