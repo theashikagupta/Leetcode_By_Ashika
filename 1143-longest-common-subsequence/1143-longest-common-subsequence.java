1bc5e61211414c31fe81845ddbaeb7b94ac6c350
Ashika Gupta <ashikagupta1503@gmail.com>
2025-12-21 13:12:41 +0530

    Time: 25 ms (29.77%), Space: 109.4 MB (5.03%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -13,16 +13,16 @@ class Solution {
         }
        
         
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    t[i][j]=1 + t[i-1][j-1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(s1.charAt(i)==s2.charAt(j)){
+                    t[i][j]=1 + t[i+1][j+1];
                 }
                 else{
-                    t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+                    t[i][j]=Math.max(t[i+1][j],t[i][j+1]);
                 }
             }
         }
-        return t[n][m];
+        return t[0][0];
     }
 }
