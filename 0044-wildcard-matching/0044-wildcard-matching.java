e97f473e3558ab90d838cade36d87f629ff727ea
Ashika Gupta <ashikagupta1503@gmail.com>
2026-01-07 21:50:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n=s.length();
+        int m=p.length();
+
+        boolean[] prev=new boolean[m+1];
+        boolean[] curr=new boolean[m+1];
+       
+        prev[0]=true;
+       
+        for(int j=1;j<m+1;j++){
+            if(p.charAt(j-1)=='*'){
+                prev[j]=prev[j-1];
+            }
+        }
+      
+         for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s.charAt(i-1)==p.charAt(j-1)||p.charAt(j-1)=='?'){
+                      curr[j]=prev[j-1];
+               }else if(p.charAt(j-1)=='*'){
+                    curr[j]=curr[j-1]||prev[j];
+                }else{
+                    curr[j]=false;
+                }
+               
+            }
+            prev=curr;
+         }
+         
+       
+        return prev[m];
+
+
+    }
+    
+}
