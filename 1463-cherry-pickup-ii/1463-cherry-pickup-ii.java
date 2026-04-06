cd87d760ff8e7ef8a355720c6a1bcc947d2d58e2
Ashika Gupta <ashikagupta1503@gmail.com>
2026-04-06 15:29:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -14,22 +14,22 @@ class Solution {
         int m = grid[0].length;
 
     
-        if (col1 < 0 || col1 >= m || col2 < 0 || col2 >= m)
+        if (col1<0 || col1>=m || col2<0 || col2>=m)
             return Integer.MIN_VALUE;
 
       
-        if (row == n - 1) {
+        if (row==n - 1) {
             if (col1 == col2)
                 return grid[row][col1];
             else
                 return grid[row][col1] + grid[row][col2];
         }
 
-        if (dp[row][col1][col2] != null)
+        if (dp[row][col1][col2]!=null)
             return dp[row][col1][col2];
 
         int cherries;
-        if (col1 == col2)
+        if (col1==col2)
             cherries = grid[row][col1];
         else
             cherries = grid[row][col1] + grid[row][col2];
@@ -37,15 +37,15 @@ class Solution {
         int max = Integer.MIN_VALUE;
 
       
-        for (int d1 = -1; d1 <= 1; d1++) {
-            for (int d2 = -1; d2 <= 1; d2++) {
-                int next = helper(row + 1, col1 + d1, col2 + d2, grid, dp);
+        for (int d1 =-1; d1<= 1; d1++) {
+            for (int d2=-1; d2<=1; d2++) {
+                int next = helper(row+1, col1+d1, col2+d2, grid, dp);
                 max = Math.max(max, next);
             }
         }
 
         cherries += max;
 
-        return dp[row][col1][col2] = cherries;
+        return dp[row][col1][col2]=cherries;
     }
 }
