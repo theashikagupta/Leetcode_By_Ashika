a1fc6943c303ad162975138bca4c982b74d9c8d5
Ashika Gupta <ashikagupta1503@gmail.com>
2026-04-06 15:18:28 +0530

    Time: 24 ms (75.41%), Space: 46.6 MB (38.86%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int cherryPickup(int[][] grid) {
+       
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Integer[][][] dp = new Integer[n][m][m];
+
+        return helper(0, 0, m - 1, grid, dp);
+    }
+
+    private int helper(int row, int col1, int col2, int[][] grid, Integer[][][] dp) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+    
+        if (col1 < 0 || col1 >= m || col2 < 0 || col2 >= m)
+            return Integer.MIN_VALUE;
+
+      
+        if (row == n - 1) {
+            if (col1 == col2)
+                return grid[row][col1];
+            else
+                return grid[row][col1] + grid[row][col2];
+        }
+
+        if (dp[row][col1][col2] != null)
+            return dp[row][col1][col2];
+
+        int cherries;
+        if (col1 == col2)
+            cherries = grid[row][col1];
+        else
+            cherries = grid[row][col1] + grid[row][col2];
+
+        int max = Integer.MIN_VALUE;
+
+      
+        for (int d1 = -1; d1 <= 1; d1++) {
+            for (int d2 = -1; d2 <= 1; d2++) {
+                int next = helper(row + 1, col1 + d1, col2 + d2, grid, dp);
+                max = Math.max(max, next);
+            }
+        }
+
+        cherries += max;
+
+        return dp[row][col1][col2] = cherries;
+    }
+}
