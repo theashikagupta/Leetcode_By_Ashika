a25bbbf00756b00e57e62f902d56804a086ba451
Ashika Gupta <ashikagupta1503@gmail.com>
2026-05-22 13:18:16 +0530

    Time: 2 ms (68.69%), Space: 59.3 MB (59.85%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let count=0;
+    let maxCount=0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]===1){
+           
+            count++;
+            maxCount=Math.max(maxCount,count);
+        }
+        else{
+            count=0;
+        }
+        
+    }
+    return maxCount;
+};
