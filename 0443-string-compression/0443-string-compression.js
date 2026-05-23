5cfe66f5017620f4fabd342e309668c09e6c457b
Ashika Gupta <ashikagupta1503@gmail.com>
2026-05-23 19:53:27 +0530

    Time: 0 ms (100%), Space: 58.5 MB (26.83%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,27 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+   let i=0;
+   let j=0;
+   while(j<chars.length){
+     let currChar=chars[j];
+     let count=0;
+     while(j<chars.length && chars[j]==currChar){
+        count++;
+        j++;
+     }
+     chars[i]=currChar;
+     i++;
+     if(count>1){
+        let countStr=count.toString();
+        for(let k=0; k<countStr.length; k++){
+          chars[i]=countStr[k];
+          i++;
+        }
+     }
+  
+   }
+    return i;
+};
