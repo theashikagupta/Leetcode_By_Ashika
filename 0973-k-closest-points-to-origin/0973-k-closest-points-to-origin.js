39de7f638584d8a7ed4323325094527f53da1d98
Ashika Gupta <ashikagupta1503@gmail.com>
2026-05-23 23:22:14 +0530

    Time: 25 ms (96.66%), Space: 68.1 MB (91.15%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+    points.sort((a,b) => (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]))
+
+    return points.slice(0,k);
+};
