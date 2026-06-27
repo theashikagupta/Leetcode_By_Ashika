223a1f8b1e60312bb319d39c49915849c60be067
Ashika Gupta <ashikagupta1503@gmail.com>
2026-06-27 17:07:25 +0530

    Time: 10 ms (41.97%), Space: 46.8 MB (37.48%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,17 +1,19 @@
-import java.util.*;
-
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
+    public boolean canConstruct(String r, String m) {
+        // if(r.length()>m.length()) return false;
+   
         Map<Character, Integer> map = new HashMap<>();
-        for (char c : magazine.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        for(int i=0; i<m.length(); i++){
+            char ch=m.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-
-        for (char c : ransomNote.toCharArray()) {
-            if (!map.containsKey(c) || map.get(c) == 0) return false;
-            map.put(c, map.get(c) - 1);
+        for(int i=0; i<r.length(); i++){
+            char ch=r.charAt(i);
+            if(!map.containsKey(ch) || map.get(ch)==0){
+               return false;
+            }
+            map.put(ch,map.get(ch)-1);
         }
-
         return true;
     }
 }
