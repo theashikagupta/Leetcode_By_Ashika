ea53ce8562fc6470fabb3e7fc8f3565b975f8c1b
Ashika Gupta <ashikagupta1503@gmail.com>
2026-06-28 10:09:09 +0530

    Time: 833 ms (58.55%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,19 +1,34 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
 
-SELECT Name AS person_name
-FROM (
-     SELECT turn 
+-- SELECT Name AS person_name
+-- FROM (
+--      SELECT turn 
+-- AS Turn,
+-- person_id AS ID,
+-- person_name AS Name,
+-- weight AS Weight,
+-- SUM(weight) OVER (ORDER BY turn) AS Total_Weight
+-- FROM Queue
+
+-- ) AS temp
+-- WHERE (Total_Weight>=1000)
+-- ORDER BY turn
+-- LIMIT 1;
+
+
+
+WITH temp AS (
+    SELECT turn 
 AS Turn,
 person_id AS ID,
 person_name AS Name,
 weight AS Weight,
 SUM(weight) OVER (ORDER BY turn) AS Total_Weight
 FROM Queue
+)
 
-) AS temp
-WHERE (Total_Weight<=1000)
+SELECT NAME AS person_name
+FROM temp
+WHERE Total_Weight<=1000
 ORDER BY turn DESC
 LIMIT 1;
-
-
-
