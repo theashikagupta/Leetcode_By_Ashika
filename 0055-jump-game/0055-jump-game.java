04d78c9d2355b4c19733c680fd8bbff966e26bf8
Ashika Gupta <ashikagupta1503@gmail.com>
2026-07-15 15:21:41 +0530

    Time: 2 ms (88.99%), Space: 48 MB (43.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,16 +2,26 @@ class Solution {
     static boolean[] dp;
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        dp=new boolean[n];
-        dp[0]=true;
-        for(int i=1; i<n; i++){
-            for(int j=i-1 ; j>=0; j--){
-                if(dp[j] && nums[j]+j>=i){
-                    dp[i]=true;
-                    break;
-                }
-            }
+        // dp=new boolean[n];
+        // dp[0]=true;
+        // for(int i=1; i<n; i++){
+        //     for(int j=i-1 ; j>=0; j--){
+        //         if(dp[j] && nums[j]+j>=i){
+        //             dp[i]=true;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return dp[n-1];
+
+        int maxReachable=0;
+
+        for(int i=0; i<n; i++){
+            if(i>maxReachable) return false;
+        
+            maxReachable=Math.max(maxReachable,i+nums[i]);
         }
-        return dp[n-1];
+        return true;
+        
       }
     }
